c3be337491fa90fb2831645d943f0050c277641f
Omer Sabah <omerrj99@gmail.com>
2021-05-21 23:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-function addingEventListener() {
-  const input = document.getElementById('input');
-input.addEventListener('click', function(event) {
-  alert('I was clicked!');
-});
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
 }
